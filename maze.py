a27bad21b188333b645081189a9defa9f4cdd6b9
Sobakiin <wgreen16@my.bcit.ca>
2021-02-28 14:36:24 -0800

    Initial commit, I suppose. Attempted to condense and streamline existing code, made notes where I made changes. Proceeding to logic porgramming.

--- maze.py
+++ maze.py
@@ -1,43 +1,67 @@
 import random
+
 class Maze:
+    """
+    Class representing the maze 
 
-    def __init__(self, file_name):
-        self._file_name=open(file_name,'r')
-        self._lines=self._file_name.readlines()
+    :Param file_name: the nameof the text file that traces the maze
+    :Type file_name: string
+    """
+    def __init__(self, file_name_):
+        #self._file_name=open(file_name,'r')
+        #self._lines=self._file_name.readlines()
+            #Not how that works
 
+        with open(file_name_,'r') as my_file:
+            lines_=my_file.readlines()
+            #print(self._lines)
+            # for item in self._lines:
+            #     print(item)
+        self._lines = lines_
+        
+        #self.items = 
 
-
-
+   
     def can_move_to(self, line_num, col_num):
-        self._line_num=line_num
-        self._col_num=col_num
-        charac = self._lines[self._line_num][self._col_num]
+        #self._line_num=line_num  
+        #self._col_num=col_num    
+            
+             # Never declare an attribute outside of the init method
+             # There wasn't a point in doing it like that anyway
+
+        #charac = self._lines[self._line_num][self._col_num]
+        charac = self._lines[line_num][col_num]
         if charac == "X":
             return False
         else:
             return True
 
-
-            
-
     def display(self):
         for item in self._lines:
             print(item)
 
-
-
-
     def find_random_spot(self):
-        dicton=[]
-        i=0
-        for line in self._lines:
-            j=0
-            for charac in line:
+        """
+        Method made to randomly select *empty spaces* in the maze
+        and place objects in them.
+
+        :Param 
+        """
+        dicton=[] #Why is it called this?
+        for i,line in enumerate(self._lines):
+            
+            for j,charac in enumerate(line):
                 if charac == " ":
                     dicton.append((i,j))
-                j=j+1
-            i=i+1
+                
         return random.choice(dicton)
+
+    #def is_item(self):
+
+if __name__=="__main__":
+    maze_file="maze.txt"
+    my_maze = Maze(maze_file)
+    my_maze.display()
 
 
 
