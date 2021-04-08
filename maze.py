9356722a422c8bcb8b52ab73e81eba4246624891
WhatUpItsDanielle <71790856+WhatUpItsDanielle@users.noreply.github.com>
2021-04-07 22:17:28 -0700

    Add files via upload

--- maze.py
+++ maze.py
@@ -1,4 +1,7 @@
 import random
+import pygame
+import pygame.locals
+
 
 class Maze:
     """
@@ -8,16 +11,39 @@ class Maze:
     :Type file_name: string
     """
     def __init__(self, file_name_):
-
+        
         with open(file_name_,'r') as my_file:
             lines_=my_file.readlines()
 
         self._lines = lines_
+        self._width = len(self._lines[0]) #grid space
+        self._height =len(self._lines)    #grid space
+    
+        self._x_scale = int(700/self._width) #pixels per grid space
+        self._y_scale = int(700/self._height)#pixels per grid space
         
-   
-    def can_move_to(self, line_num, col_num):
+        self.player = 0
+        self._items = []
+
+        self.surface = pygame.Surface((800,800))
+    
+    @property
+    def surface(self):
+        return self._surface
+
+    @surface.setter
+    def surface(self,value):
+        self._surface = value
+
+    def can_move_to(self, col_num, line_num):
         """
         Method dictating what spaces can be walked through
+
+        :param col_num: the column of the location being checked
+        :type col_num: int
+
+        :param line_num: the line of the location being checked
+        :type line_num: int
 
         :returns: bool
         """
@@ -30,26 +56,55 @@ class Maze:
 
     def display(self):
         """
-        Method to display the maze made from the text file
+        Method to create the surface of the maze made from the text file
         prints image based on coordinates
         """
         items = [self.find_random_spot() for i in range(4)]
-        print(items)
+        
         while(len(set(items))!=4):
             items = list(set(items))
             items.append(self.find_random_spot())
-        print(items)
         
+        surface = pygame.Surface((700,700))
 
-        for x,line in enumerate(self._lines):
-            for y,space in enumerate(line):
+        for y,line in enumerate(self._lines):
+            for x,space in enumerate(line):
+
+
                 if (self.is_item((x,y), items)):
-                    print("O",end="") 
-                if (self.is_exit(x,y)):
-                    print("E",end="")
+                    # item = pygame.Surface((self._x_scale,self._y_scale))
+                    # pygame.draw.circle(item,(250,250,250),(self._x_scale/2,self._y_scale/2),(self._x_scale/4))
+                    item = pygame.Surface((self._x_scale,self._y_scale))
+                    item.fill((0,0,0))
+
+                    self._items.append([x*self._x_scale,y*self._y_scale,self._x_scale,self._y_scale])
+                
+                elif (self.is_exit(x,y)):
+                    
+                    item = pygame.Surface((self._x_scale,self._y_scale))
+                    item.fill((255,255,0))
+                    
+                elif (self.is_player(x,y)):
+                    item = pygame.Surface((self._x_scale,self._y_scale))
+                    item.fill((0,0,0))
+
+                    self.player = ([x*self._x_scale,y*self._y_scale,self._x_scale,self._y_scale])
+                    
+
+                elif not(self.can_move_to(x,y)):
+                    item = pygame.image.load("Views\Wall.png")
+                    #item = pygame.Surface((self._x_scale,self._y_scale))
+                    #item.fill((250,0,0))
+
                 else:
-                    print(self._lines[x][y],end="")
-            print("")
+                    
+                    item = pygame.Surface((self._x_scale,self._y_scale))
+                    item.fill((0,0,0))
+
+                surface.blit(item,(x*self._x_scale,y*self._y_scale))
+                
+
+        self.surface = surface
 
     def find_random_spot(self):
         """
@@ -62,7 +117,7 @@ class Maze:
             
             for j,charac in enumerate(line):
                 if charac == " ":
-                    dicton.append((i,j))
+                    dicton.append((j,i))
                 
         return random.choice(dicton)
 
@@ -85,7 +140,7 @@ class Maze:
             
         return False
 
-    def is_exit(self, line_num, col_num):
+    def is_exit(self, col_num, line_num):
         """
         Checks a spot on the grid while it's generating to see 
         if its coordinates match those of the exit point
@@ -104,14 +159,59 @@ class Maze:
         else:
             return False
 
+    def is_player(self,col,line):
 
-    # window = pygame.display.set_mode((500, 500))
-    # window.fill((100, 100, 100))
-    # pygame.display.flip()
-    """Pygame initialization commented out. Uncomment when attempting to implement the game screen """
+        charac = self._lines[line][col]
+        if charac == "P":
+            return True
+        else:
+            return False
+
+class Items(pygame.sprite.Sprite):
+    """
+    Class representing the items on the floor of the maze
+    through a sprite
+
+    :param x: the pixel location of the sprite along the x axis
+    :type x: int
     
-    maze_file="maze.txt"
-    my_maze = Maze(maze_file)
+    :param y: the pixel location of the sprite along the y axis
+    :type y: int
     
-    my_maze.display()
+    :param scales: a set of scales to help center the sprite on the maze grid
+    :type scales: list
+    """
+
+    def __init__(self,x,y,scales):
+        super().__init__()
+        image = pygame.image.load("Views\item.png")
+        self.image = pygame.transform.scale(image, (25, 25))
+        self.rect = self.image.get_rect()
+        self.rect.x=x+scales[0]/2
+        self.rect.y=y+scales[1]/2
+
+
+if __name__ == "__main__":
+    pygame.init()
     
+    width, height = 800, 800
+    window = pygame.display.set_mode((width, height))
+    window.fill((250,250,250))
+    
+    run = True
+
+    pygame.display.set_caption("Maze Game")
+    maze=Maze("maze.txt")
+    maze.display()
+    #player = maze.player
+
+    print(type(maze.surface))
+    window.blit(maze.surface,(10,10))
+
+    pygame.display.flip()
+
+    while run:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+        
